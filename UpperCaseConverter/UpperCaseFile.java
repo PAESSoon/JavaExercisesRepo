54cdf81a13339b9055740a157a44b650682d24b6
Pamela S-S <atheanae@gmail.com>
2022-01-25 08:33:10 -0500

    UpperCase - add write to file output

--- UpperCaseConverter/UpperCaseFile.java
+++ UpperCaseConverter/UpperCaseFile.java
@@ -27,16 +27,22 @@ public class UpperCaseFile {
     public static void convertToUpperCase() {
     	
     	BufferedReader br = null;
+    	BufferedWriter bw = null;
+//    	FileWriter(String filename, boolean append);
 
 		try {
 
 			File fileInput = new File(fileRead);
 
 			br = new BufferedReader(new FileReader(fileInput));
+			
+			FileWriter writer = new FileWriter(fileWrite, true);
+			
+			bw = new BufferedWriter(writer);
 
 			String strInput;
 
-			String[] inputFromBuffer = new String[4];
+			String[] inputFromBuffer = new String[5];
 
 			int i = 0;
 
@@ -44,27 +50,30 @@ public class UpperCaseFile {
 
 				inputFromBuffer[i] = strInput;
 
+				
+				String userInput = inputFromBuffer[i];
+				
+				for (int j = 0; j < userInput.length(); j++) {
+					
+					
+					
+					if(userInput.charAt(j) != 'a' && userInput.charAt(j) != 'e' && userInput.charAt(j) != 'i' && userInput.charAt(j) != 'o' && userInput.charAt(j) != 'u') {
+						userInput = userInput.replace(userInput.charAt(j), Character.toUpperCase(userInput.charAt(j)));
+//						System.out.println("userInput.charAt(j): " + userInput.charAt(j));
+					}
+				}
+
+				bw.write(userInput + "\n");
+				
+				System.out.println("After conversion: " + i + " " + userInput);
+				
 				i++;
 			}
 
-			String userInput = inputFromBuffer[0];
-
-			// constructs a Character instance
-			System.out.println("Before conversion: " + userInput);
-
 			i = 0;
 			
-			for (int j = 0; j < userInput.length(); j++) {
-				
-				
-				
-				if(userInput.charAt(j) != 'a' && userInput.charAt(j) != 'e' && userInput.charAt(j) != 'i' && userInput.charAt(j) != 'o' && userInput.charAt(j) != 'u') {
-					userInput = userInput.replace(userInput.charAt(j), Character.toUpperCase(userInput.charAt(j)));
-					System.out.println("userInput.charAt(j): " + userInput.charAt(j));
-				}
-			}
-			
-			System.out.println("After conversion: " + userInput);
+			bw.close();
+
 
 		} catch (FileNotFoundException e) {
 
