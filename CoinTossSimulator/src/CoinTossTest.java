fb6d864d11139d40b328cd386444c05311880c38
Pamela S-S <atheanae@gmail.com>
2022-01-25 09:49:43 -0500

    CoinToss - add coin toss action simulation

--- CoinTossSimulator/src/CoinTossTest.java
+++ CoinTossSimulator/src/CoinTossTest.java
@@ -7,6 +7,24 @@ public class CoinTossTest {
 		
 		System.out.println("Side facing up at Start: " + dollar.getSideUp());
 		
+		int headCount = 0, tailCount = 0;
+		
+		for (int i = 0; i < 20; i++) {
+			
+			dollar.toss();
+			System.out.println("Side facing up: " + dollar.getSideUp());
+			
+			if(dollar.getSideUp().equals("Head")) {
+				
+				headCount++;
+			} else {
+				
+				tailCount++;
+			}
+		}
+		
+		System.out.println("Head Count: " + headCount);
+		System.out.println("Tail Count: " + tailCount);
 
 	}
 
